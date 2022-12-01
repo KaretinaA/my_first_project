f88c7e69bf2e1b8165fb8072b9038f5564e59091
KaretinaA <aleksanna-92@mail.ru>
2022-12-01 23:42:26 +0300

    homework2_cat

---
+++ src/lesson3/Cat.java
@@ -0,0 +1,21 @@
+package lesson3;
+
+public class Cat {
+        private String name;
+        int age;
+        Cat(String name, int age) {
+            this.name = name;
+            this.age = age;
+        }
+       public String getName() {
+            return name; }
+
+        public void setName(String name) {
+         //   if (name == "Dany" || name == "Mars") {
+                this.name = name;
+            }
+         //   else {
+            //    System.out.println("Ошибка! Имя нельзя менять");
+
+
+}
